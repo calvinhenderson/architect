8d57d1a459b03de14674ae2d31418f4710ef0168
Calvin Henderson <henderson.calvin.w@gmail.com>
2026-04-22 14:19:47 -0400

    reworked blueprints to rely on schemas and not structs

--- lib/architect/blueprints.ex
+++ lib/architect/blueprints.ex
@@ -1,2 +1,18 @@
 defmodule Architect.Blueprints do
+  @moduledoc """
+  """
+
+  import Architect.Schemas, only: [validate_data: 2]
+
+  @type blueprint :: map()
+
+  @doc "Evaluates Blueprint YAML definitions"
+  @spec evaluate_blueprint(blueprint(), keyword()) ::
+          {:ok, blueprint()} | {:error, atom(), term()}
+  def evaluate_blueprint(blueprint, _opts \\ []) do
+    case validate_data("schemas/blueprint", blueprint) do
+      {:ok, validated} -> {:ok, validated}
+      {:error, reason, term} -> {:error, reason, term}
+    end
+  end
 end
--- test/architect/blueprints_test.exs
+++ test/architect/blueprints_test.exs
@@ -1,10 +1,27 @@
-defmodule Architect.BlueprintsTest do
+defmodule Architect.Blueprints.EvaluatorTest do
   use Architect.DataCase
 
   import Architect.Blueprints
 
+  @valid_blueprint_params %{
+    "id" => "blueprint_evaluation",
+    "namespace" => "testing",
+    "tasks" => []
+  }
+
+  @invalid_blueprint_params %{}
+
   describe "evaluate/2" do
-    test "validates the blueprint"
-    test "returns errors when the blueprint is invalid"
+    test "validates the blueprint" do
+      assert {:error, :validate_schema_data, errors} =
+               evaluate_blueprint(@invalid_blueprint_params)
+
+      assert {"Required properties id, namespace, tasks were not present.", "#"} in errors
+    end
+
+    test "returns a validated blueprint" do
+      params = @valid_blueprint_params
+      assert {:ok, ^params} = evaluate_blueprint(params)
+    end
   end
 end
