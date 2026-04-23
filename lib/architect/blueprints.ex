6920aa4e005d33ff12f95ab0fbb88db1984d3b76
Calvin Henderson <henderson.calvin.w@gmail.com>
2026-04-23 08:57:58 -0400

    added support for evaluating yaml blueprints

--- lib/architect/blueprints.ex
+++ lib/architect/blueprints.ex
@@ -4,12 +4,21 @@ defmodule Architect.Blueprints do
 
   import Architect.Schemas, only: [validate_data: 2]
 
-  @type blueprint :: map()
+  @type blueprint :: binary() | map()
 
   @doc "Evaluates Blueprint YAML definitions"
   @spec evaluate_blueprint(blueprint(), keyword()) ::
           {:ok, blueprint()} | {:error, atom(), term()}
-  def evaluate_blueprint(blueprint, _opts \\ []) do
+  def evaluate_blueprint(blueprint, opts \\ [])
+
+  def evaluate_blueprint(blueprint, opts) when is_binary(blueprint) do
+    blueprint
+    |> Architect.Documents.load_yaml(opts)
+    |> List.first(%{})
+    |> evaluate_blueprint(opts)
+  end
+
+  def evaluate_blueprint(blueprint, _opts) do
     case validate_data("schemas/blueprint", blueprint) do
       {:ok, validated} -> {:ok, validated}
       {:error, reason, term} -> {:error, reason, term}
--- test/architect/blueprints_test.exs
+++ test/architect/blueprints_test.exs
@@ -19,9 +19,34 @@ defmodule Architect.Blueprints.EvaluatorTest do
       assert {"Required properties id, namespace, tasks were not present.", "#"} in errors
     end
 
-    test "returns a validated blueprint" do
+    test "evaluates valid yaml document" do
       params = @valid_blueprint_params
       assert {:ok, ^params} = evaluate_blueprint(params)
+    end
+
+    test "evalulates a valid yaml blueprint" do
+      params = @valid_blueprint_params
+
+      valid_document = """
+      id: blueprint_evaluation
+      namespace: testing
+      tasks: []
+      """
+
+      assert {:ok, ^params} = evaluate_blueprint(valid_document)
+    end
+
+    test "returns schema errors for invalid yaml document" do
+      invalid_document = """
+      ---
+      invalid: invalid
+      """
+
+      assert {:error, :validate_schema_data, errors} =
+               invalid_document
+               |> evaluate_blueprint()
+
+      assert {"Schema does not allow additional properties.", "#/invalid"} in errors
     end
   end
 end
